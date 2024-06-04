b2502a7f6a6c81093375c607174298372c6335ee
Paul Garton <paulgarton1984@hotmail.com>
2024-06-04 20:38:28 +0100

    import json data file and render in table

--- applications_tracker/src/TableOfEntries/TableOfEntries.js
+++ applications_tracker/src/TableOfEntries/TableOfEntries.js
@@ -1,35 +1,24 @@
 import Table from "react-bootstrap/Table";
+import data from "../data.json";
 
 function TableOfEntries() {
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>Company</th>
+          <th>Location</th>
+          <th>Tech Stack</th>
         </tr>
       </thead>
       <tbody>
-        <tr id="companyName">
-          <td>1</td>
-          <td>Microsoft</td>
-          <td>Amazon</td>
-          <td>@Google</td>
-        </tr>
-        <tr id="location">
-          <td>2</td>
-          <td>Remote</td>
-          <td>Derby</td>
-          <td>London</td>
-        </tr>
-        <tr id="techStack">
-          <td>3</td>
-          <td>C#</td>
-          <td>React</td>
-          <td>JavaScript</td>
-        </tr>
+        {data.companies.map((comp) => (
+          <tr key={comp.id}>
+            <td>{comp.company}</td>
+            <td>{comp.location}</td>
+            <td>{comp.techStack}</td>
+          </tr>
+        ))}
       </tbody>
     </Table>
   );
